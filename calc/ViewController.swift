eaa4004419ffbe704b4fc68ab9575fcf8327e06f
Ravi627 <chandakravi627@gmail.com>
2017-07-20 06:18:50 +0530

    Initial Commit

---
+++ calc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  calc
+//
+//  Created by Ravi Chandak on 20/07/17.
+//  Copyright © 2017 Ravi Chandak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
