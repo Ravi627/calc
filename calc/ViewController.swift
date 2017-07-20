174240641b419dfa4a01394c4d1a429a830a9b63
Ravi627 <chandakravi627@gmail.com>
2017-07-20 21:18:23 +0530

    numpad functional

--- calc/ViewController.swift
+++ calc/ViewController.swift
@@ -9,17 +9,69 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var textfeild: UILabel!
+    var counter:Int32 = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+ 
+    @IBAction func clicked (click: UIButton)
+    {
+        if let num = click.titleLabel?.text{
+            
+       
+            if textfeild.text == "0"{
+            textfeild.text = click.titleLabel?.text
+           counter = 1;
+            }
+        
+            else{
+                
+               if counter < 10{
+                    textfeild.text = textfeild.text! + num ;
+                   counter+=1 ;
+               }
+        
+            }
+        }
+        print(counter)
+    }
+    
+    @IBAction func plus (click: UIButton)
+    {
+        if let sign = click.titleLabel?.text{
+        print(sign)
+        }
+    }
+    
+    @IBAction func minus (click: UIButton)
+    {
+        if let sign = click.titleLabel?.text{
+            print(sign)
+        }
+    }
+    
+    @IBAction func multi (click: UIButton)
+    {
+        if let sign = click.titleLabel?.text{
+            print(sign)
+        }
+    }
+    
+    @IBAction func divide (click: UIButton)
+    {
+        if let sign = click.titleLabel?.text{
+            print(sign)
+        }
+    }
 }
 
+    
